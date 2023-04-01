0640ae24c48a8f92afb8124b97a90cbdb0e58923
mehmetCt <mehmet@cybertekschool.com>
2023-04-01 16:00:26 -0400

    P04

---
+++ src/test/java/com/cydeo/week1/P04_QueryParam.java
@@ -0,0 +1,21 @@
+package com.cydeo.week1;
+
+public class P04_QueryParam {
+
+    /**
+     *1- Given accept type is Json
+     *2- Query Parameters values are
+     *     - minprice —> 0
+     *     - maxprice —> 0.1
+     *3- When user sends GET request to /activity
+     *4- Print out Followings
+     *     - response
+     *     - activity
+     *     - type
+     *     - participants
+     * 5- Verify followings
+     *     - Status code should be 200
+     *     - price is lower than 0.1
+
+     */
+}
