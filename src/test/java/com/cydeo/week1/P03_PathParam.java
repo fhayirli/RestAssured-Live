0f7ddc34c0667a35f29f8555d62e37d010e2b2b6
mehmetCt <mehmet@cybertekschool.com>
2023-04-01 15:20:17 -0400

    P03

---
+++ src/test/java/com/cydeo/week1/P03_PathParam.java
@@ -0,0 +1,19 @@
+package com.cydeo.week1;
+
+import com.cydeo.utility.FruitAPITestBase;
+
+public class P03_PathParam extends FruitAPITestBase {
+
+    /**
+     *1- Given accept type is Json
+     *2- Path Parameters value is
+     *     - id —> Fruits
+     *3- When user sends GET request to /categories/{id}
+     *4- Verify followings
+     *     - Status code should be 200
+     *     - Content Type is application/json; charset=utf-8
+     *     - Print response
+     *     - Name is "Fruits"
+     *
+     */
+}
