bc870cf69aec933892ed7644b535f7f353f89419
mehmetCt <mehmet@cybertekschool.com>
2023-04-01 14:37:55 -0400

    P02

---
+++ src/test/java/com/cydeo/utility/HRTestBase.java
@@ -0,0 +1,21 @@
+package com.cydeo.utility;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+public abstract class HRTestBase {
+
+    @BeforeAll
+    public static void init(){
+
+
+
+    }
+
+    @AfterAll
+    public static void destroy(){
+
+
+
+    }
+}
