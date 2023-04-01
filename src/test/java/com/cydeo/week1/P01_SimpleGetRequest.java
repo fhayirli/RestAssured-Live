76eeba5229a7b27a48a2a326608d7e1e151ea7bf
mehmetCt <mehmet@cybertekschool.com>
2023-04-01 11:50:17 -0400

    Initial for RestAssured-Live

---
+++ src/test/java/com/cydeo/week1/P01_SimpleGetRequest.java
@@ -0,0 +1,19 @@
+package com.cydeo.week1;
+
+public class P01_SimpleGetRequest {
+
+/**
+ * 1. Send request to HR url and save the response
+ * 2. GET /regions
+ * 3. Store the response in Response Object that comes from GET Request
+ * 4. Print out followings
+ *     - Headers
+ *     - Content-Type
+ *     - Status Code
+ *     - Response
+ *     - Date
+ *     - Verify response body has "Europe"
+ *     - Verify response has Date
+ */
+
+}
