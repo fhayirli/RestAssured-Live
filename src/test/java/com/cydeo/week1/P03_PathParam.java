03baf23f627ac4284a052352f835cedd9eec72ca
mehmetCt <mehmet@cybertekschool.com>
2023-04-01 15:23:59 -0400

    P03

--- src/test/java/com/cydeo/week1/P03_PathParam.java
+++ src/test/java/com/cydeo/week1/P03_PathParam.java
@@ -1,7 +1,12 @@
 package com.cydeo.week1;
 
 import com.cydeo.utility.FruitAPITestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
 public class P03_PathParam extends FruitAPITestBase {
 
     /**
@@ -16,4 +21,17 @@ public class P03_PathParam extends FruitAPITestBase {
      *     - Name is "Fruits"
      *
      */
+
+    @Test
+    public void getSingleCategory() {
+
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/shop/categories/Fruits");
+
+        response.prettyPrint();
+
+
+
+
+    }
 }
