8059df5b25a20deee2ba52e2c53d7f4a574e915d
OscarSah <oscar@cybertekschool.com>
2023-04-06 03:00:24 +0300

    Evening Session Begins- NOTES shared

---
+++ src/test/java/com/cydeo/officehours/evening/Task01.java
@@ -0,0 +1,19 @@
+package com.cydeo.officehours.evening;
+
+public class Task01 {
+    /**
+     * Send request to HR API URL  and save the response
+     * Path Param job_id is IT_PROG
+     * GET /jobs/{job_id}
+     * Store the response in Response Object that comes from get Request
+     * Verify followings
+     *     - Print response
+     *     - Status Code is 200
+     *     - Content-Type is application/json
+     *     - job_id is IT_PROG
+     *     - job_title is Programmer
+     *     - Get me first rel information
+     *     - Get me 2,3 rel information
+     * 	   - Get me all rel information
+     */
+}
