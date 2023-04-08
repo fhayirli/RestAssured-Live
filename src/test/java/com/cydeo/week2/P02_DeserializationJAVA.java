7f977067c9a09cffaa731354ce82130a549fb2de
mehmetCt <mehmet@cybertekschool.com>
2023-04-08 11:33:06 -0400

    Live Session Week 2

--- src/test/java/com/cydeo/week2/P02_DeserializationJAVA.java
+++ src/test/java/com/cydeo/week2/P02_DeserializationJAVA.java
@@ -1,7 +1,11 @@
 package com.cydeo.week2;
 
 import com.cydeo.utility.CydeoTrainingTestBase;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,7 +28,25 @@ public class P02_DeserializationJAVA extends CydeoTrainingTestBase {
     public void deserializeJAVA() {
 
 
+        Response response = given().log().uri().
+                when().get("/teacher/all").prettyPeek().
+                then().statusCode(200)
+                .extract().response();
 
+
+        // response.as("teachers[0]", Map.class);
+        // response.as method doest not allow to retrieve partil part of JSON by providing GPATH
+        // That is why we are not eligible to do it
+        //     * System.out.println("====== GET FIRST TEACHER INFO AS MAP  ======");
+        //     * System.out.println("====== GET FIRST TEACHER NAME  ======");
+
+
+        Map<String,Object> allTeachers = response.as(Map.class);
+        System.out.println("allTeachers = " + allTeachers);
+        //     * System.out.println("====== GET ALL TEACHER INFO  AS LIST OF MAP======");
+        //     * System.out.println("====== FIRST TEACHER INFO======");
+        //     * System.out.println("====== FIRST TEACHER NAME ======");
+        //     * System.out.println("====== LAST TEACHER NAME  ======");
 
 
 
