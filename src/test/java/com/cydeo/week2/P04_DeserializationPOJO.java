59b2dfbe1146b2eed964292a1a5b0add0ff5ff41
mehmetCt <mehmet@cybertekschool.com>
2023-04-08 15:26:24 -0400

    Live Session Week 2

--- src/test/java/com/cydeo/week2/P04_DeserializationPOJO.java
+++ src/test/java/com/cydeo/week2/P04_DeserializationPOJO.java
@@ -1,6 +1,9 @@
 package com.cydeo.week2;
 
+import com.cydeo.pojo.CustomerData;
 import com.cydeo.utility.FruitAPITestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -21,7 +24,36 @@ public class P04_DeserializationPOJO extends FruitAPITestBase {
     @Test
     public void deserialization() {
 
+        JsonPath jp = given().log().uri()
+                .accept(ContentType.JSON).
+                when().get("/shop/customers/").prettyPeek().
+                then().statusCode(200)
+                .extract().jsonPath();
 
+        CustomerData customerData = jp.getObject("", CustomerData.class);
+
+        System.out.println(customerData);
+
+        System.out.println("----- PRINT COUNT ----");
+
+        System.out.println("----- PRINT LIMIT ----");
+
+        System.out.println("----- PRINT NEXTURL ----");
+        String nextUrl = customerData.getMeta().getNextUrl();
+
+
+        while(nextUrl!=null){
+
+             jp = get(nextUrl).prettyPeek()
+                    .then().statusCode(200)
+                    .extract().jsonPath();
+
+
+
+             // To update each time current response nextURL infromation
+             nextUrl = jp.getString("meta.next_url");
+
+        }
 
 
     }
