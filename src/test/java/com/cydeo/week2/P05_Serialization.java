718f80863609a14acbc9fa94ad02e7469b62993e
mehmetCt <mehmet@cybertekschool.com>
2023-04-08 15:36:56 -0400

    Live Session Week 2

--- src/test/java/com/cydeo/week2/P05_Serialization.java
+++ src/test/java/com/cydeo/week2/P05_Serialization.java
@@ -8,6 +8,19 @@ import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 public class P05_Serialization extends FruitAPITestBase {
 
+      /*
+    Given accept header is application json
+    And content type is application json
+    And requestBody field and values are
+        |firstname|lastname|
+        |Mike     |Smith   |
+    When I send POST request /shop/customers endpoint
+    Then status code should be 201
+    And response body should have posted customer information
+
+
+     */
+
     @Test
    public  void POSTwithMAP() {
     }
