83515a507e33c6a786690c4ab62f0be527e44157
mehmetCt <mehmet@cybertekschool.com>
2023-04-08 17:50:40 -0400

    Live Session Week 2

---
+++ src/test/java/com/cydeo/week2/P06_CurrentsAPIKEY.java
@@ -0,0 +1,32 @@
+package com.cydeo.week2;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class P06_CurrentsAPIKEY {
+    String apiKey="a6v39P9aam1Q7YaB-TS5u34p_AdSejb5RJoke0lA8YQIV6Ac";
+    // Put config.properties
+    @Test
+    public void apiKeyQP() {
+
+
+        given().log().uri().accept(ContentType.JSON)
+                .baseUri("https://api.currentsapi.services/v1")
+                .queryParam("apiKey","a6v39P9aam1Q7YaB-TS5u34p_AdSejb5RJoke0lA8YQIV6Ac").
+        when().get("/latest-news").prettyPeek();
+
+
+    }
+
+    @Test
+    public void apiKeyAuthHeader() {
+
+        given().log().uri().accept(ContentType.JSON)
+                .baseUri("https://api.currentsapi.services/v1")
+                .header("Authorization","a6v39P9aam1Q7YaB-TS5u34p_AdSejb5RJoke0lA8YQIV6Ac").
+                when().get("/latest-news").prettyPeek();
+
+    }
+}
