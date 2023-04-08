e0f3b6b0010a55c98acafa6afb1529e510762966
mehmetCt <mehmet@cybertekschool.com>
2023-04-08 11:17:38 -0400

    Live Session Week 2

---
+++ src/test/java/com/cydeo/week2/P02_DeserializationJAVA.java
@@ -0,0 +1,25 @@
+package com.cydeo.week2;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class P02_DeserializationJAVA {
+
+    /**
+     * Create a test called getTeachers
+     * 1. Send request to GET /teacher/all
+     * 2. log uri to see
+     * 3. Get all Json as a map and print out screen all the things with the help of  map
+     * System.out.println("====== GET FIRST TEACHER INFO AS MAP  ======");
+     * System.out.println("====== GET FIRST TEACHER NAME  ======");
+     * System.out.println("====== GET ALL TEACHER INFO  AS LIST OF MAP======");
+     * System.out.println("====== FIRST TEACHER INFO======");
+     * System.out.println("====== FIRST TEACHER NAME ======");
+     * System.out.println("====== LAST TEACHER NAME  ======");
+     */
+
+    @Test
+    public void deserializeJAVA() {
+    }
+}
