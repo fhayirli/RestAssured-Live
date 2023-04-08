7dd71a320948785641c45d4cfbbe5d4bec3b35bc
mehmetCt <mehmet@cybertekschool.com>
2023-04-08 12:24:46 -0400

    Live Session Week 2

---
+++ src/test/java/com/cydeo/week2/P03_DeserializationPOJO.java
@@ -0,0 +1,15 @@
+package com.cydeo.week2;
+
+public class P03_DeserializationPOJO {
+
+    /*
+        - ERGAST API
+        - Given accept type is json
+        - When user send request /status.json
+        - Then verify status code is 200
+        - And content type is application/json; charset=utf-8
+        - And total is 137
+        - And limit is 30
+        - And each status has statusId
+     */
+}
