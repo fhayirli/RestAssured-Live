bc123ccff3e635eef4566adf41bceff3937855a9
mehmetCt <mehmet@cybertekschool.com>
2023-04-08 14:37:45 -0400

    Live Session Week 2

--- src/test/java/com/cydeo/week2/P03_DeserializationPOJO.java
+++ src/test/java/com/cydeo/week2/P03_DeserializationPOJO.java
@@ -1,8 +1,12 @@
 package com.cydeo.week2;
 import com.cydeo.pojo.MRData;
+import com.cydeo.pojo.Status;
 import com.cydeo.utility.FormulaAPITestBase;
 import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -31,6 +35,34 @@ public class P03_DeserializationPOJO extends FormulaAPITestBase {
 
         MRData mrData = jsonPath.getObject("MRData", MRData.class);
         System.out.println(mrData);
+
+        //        - And total is 137
+        Assertions.assertEquals("137",mrData.getTotal());
+
+        //        - And limit is 30
+        Assertions.assertEquals("30",mrData.getLimit());
+
+        //        - And each status has statusId
+
+
+        System.out.println("------- STATUS TABLE -------");
+        System.out.println(mrData.getStatusTable());
+
+        System.out.println("------- STATUS LIST -------");
+        List<Status> allStatus = mrData.getStatusTable().getStatus();
+        for (Status status : allStatus) {
+            System.out.println(status);
+        }
+
+        System.out.println("------- FIRST STATUS DETAILS  -------");
+
+        // mrData.getStatusTable().getStatus().get(0) ---> it will give first status as OBJECT
+        Status firstStatus = allStatus.get(0);
+
+        System.out.println(firstStatus.getStatusId());
+        System.out.println(firstStatus.getCount());
+        System.out.println(firstStatus.getStatus());
+
 
     }
 }
