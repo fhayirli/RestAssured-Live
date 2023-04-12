28f00b870b9c28302c06e1c1b11ca9fe11ab0e71
mehmetCt <mehmet@cybertekschool.com>
2023-04-12 12:08:41 -0400

    OH Week 2

--- src/test/java/com/cydeo/officehours/morning/week2/P01_LibraryLoginTest.java
+++ src/test/java/com/cydeo/officehours/morning/week2/P01_LibraryLoginTest.java
@@ -1,13 +1,36 @@
 package com.cydeo.officehours.morning.week2;
 
+import com.cydeo.utility.LibraryTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
-public class P01_LibraryLoginTest {
+public class P01_LibraryLoginTest extends LibraryTestBase {
 
     @Test
     public void loginTest() {
+
+
+        JsonPath jp = given().log().uri()
+                .contentType(ContentType.URLENC) // same with application/x-www-form-urlencoded
+                .formParam("email", "librarian10@library")
+                .formParam("password", "libraryUser").
+                // This is how to send body with URLENCODED format
+                        when().post("/login").prettyPeek().
+                then().statusCode(200)
+                .extract().jsonPath();
+
+
+        // How to extract token information
+        String token = jp.getString("token");
+        System.out.println(token);
+
+
+        // Send get request to retrieve dashboard_stats
+
+
     }
 }
