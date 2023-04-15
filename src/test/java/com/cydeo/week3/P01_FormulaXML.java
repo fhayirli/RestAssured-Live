ed53a9c37a57a3246577ab285e95b71d00b739d0
mehmetCt <mehmet@cybertekschool.com>
2023-04-15 10:37:40 -0400

    Live Week 3

---
+++ src/test/java/com/cydeo/week3/P01_FormulaXML.java
@@ -0,0 +1,26 @@
+package com.cydeo.week3;
+
+import com.cydeo.utility.FormulaAPITestBase;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class P01_FormulaXML extends FormulaAPITestBase {
+
+      /*
+    - Send GET Request /drivers
+           // first Driver Given Name
+           // all Drivers Given Name
+           // print out first driver driverId attribute
+           // print out all driver driverId attribute
+           // Print out all driver given name if their nationality is Italian
+
+     */
+
+    @Test
+    public void xmlPractice() {
+
+
+    }
+}
