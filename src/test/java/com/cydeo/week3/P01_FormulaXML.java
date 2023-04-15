75c68cc8c33e7b15d87c677dcb3b05e54c6b16fb
mehmetCt <mehmet@cybertekschool.com>
2023-04-15 11:00:42 -0400

    Live Week 3

--- src/test/java/com/cydeo/week3/P01_FormulaXML.java
+++ src/test/java/com/cydeo/week3/P01_FormulaXML.java
@@ -43,7 +43,7 @@ public class P01_FormulaXML extends FormulaAPITestBase {
         System.out.println("allGivenNames = " + allGivenNames);
 
         //           // print out first driver driverId attribute
-        String firstDriverID = xmlPath.getString("MRData.DriverTable.Driver[0].@driverId");
+        String firstDriverID = xmlPath.getString("MRData.DriverTable.Driver[i].@driverId");
         System.out.println("firstDriverID = " + firstDriverID);
 
         //           // print out all driver driverId attribute
@@ -51,6 +51,41 @@ public class P01_FormulaXML extends FormulaAPITestBase {
         System.out.println("allDriverIDs = " + allDriverIDs);
 
         //           // Print out all driver given name if their nationality is Italian
+        List<String> allItalians = xmlPath.getList("MRData.DriverTable.Driver.findAll {it.Nationality=='Italian'}.GivenName");
+        System.out.println("allItalians = " + allItalians);
 
+
+        /*
+
+        Q --> How to create POJO to store all driver information ?
+
+        - Create a POJO class
+            GivenName
+            Natiaonality
+            DOB
+            FamilyName
+
+        - Since it is not a OBJECT just like JSON
+             <Driver driverId="acheson" url="http://en.wikipedia.org/wiki/Kenny_Acheson">
+                <GivenName>Kenny</GivenName>
+                <FamilyName>Acheson</FamilyName>
+                <DateOfBirth>1957-11-27</DateOfBirth>
+                <Nationality>British</Nationality>
+              </Driver>
+
+        - It is not possible to do Deserializaton to POJO by using JACKSON
+
+        - First find size of drivers --> 10
+
+        Create LOOP i =0 ; i<10
+            Create POJO Driver Object
+                DriverPOJO driver=new DriverPOJO();
+                driver.setGivenName(xmlPath.getString("MRData.DriverTable.Driver["+i+"].GivenName"));
+                driver.setDriverID(xmlPath.getString("MRData.DriverTable.Driver["+i+"].@driverId"));
+                driver.setFamilyName(xmlPath.getString("MRData.DriverTable.Driver["+i+"].FamilyName"))
+                ...
+                ..
+
+         */
     }
 }
