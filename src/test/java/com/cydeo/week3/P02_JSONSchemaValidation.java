50337ee0a86d88f1768115eca7aa768077827e93
mehmetCt <mehmet@cybertekschool.com>
2023-04-15 11:37:26 -0400

    Live Week 3

--- src/test/java/com/cydeo/week3/P02_JSONSchemaValidation.java
+++ src/test/java/com/cydeo/week3/P02_JSONSchemaValidation.java
@@ -1,13 +1,24 @@
 package com.cydeo.week3;
 
 import com.cydeo.utility.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.*;
 
 public class P02_JSONSchemaValidation extends SpartanTestBase {
 
 
+    @Test
+    public void allSpartansSchemaValidation() {
 
+        given().log().uri()
+                .accept(ContentType.JSON).
+        when().get("/spartans").prettyPeek().
+        then().log().ifValidationFails()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("AllSpartansSchema.json"));
 
-
-
+    }
 }
